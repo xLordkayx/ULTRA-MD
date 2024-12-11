77db86068405d09ad5b38bf2b36a638345748719
xLordkayx <westwalley061@gmail.com>
2024-12-11 20:34:03 +0100

    Update config.js

--- config.js
+++ config.js
@@ -10,7 +10,7 @@ const defaultOwner = '2347045505096';
 
 
 // Check for the OWNERS environment variable; if not found, use the default
-const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || 'your number';  // put your number here
+const ownervb = process.env.OWNERS || process.env.OWNER_NUMBER || '2347045505096';  // put your number here
 
 const ownerlist = ownervb.split(';');
 
